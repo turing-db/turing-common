a2677a9447d5951f2d7d7e29371f54dcfb3126b5
Remy Boutonnet <remy.boutonnet@turing.bio>
2025-07-24 18:51:49 +0200

    Update for build with clang

--- lib/FastGet.h
+++ lib/FastGet.h
@@ -6,6 +6,10 @@
 
 template <typename Type, typename... ArgsT>
 inline constexpr auto& FastGet(std::variant<ArgsT...>& variant) {
+#if defined(__clang__)
+    return std::get<Type>(variant);
+#else
     constexpr size_t index = PackIndexOfType<Type, ArgsT...>::get();
     return std::__detail::__variant::__get<index>(variant);
+#endif
 }
