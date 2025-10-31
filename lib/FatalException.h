f09a0ea7ab698ffbf21b16fd9e6917dab1297828
cyrusknopf <cyrus.knopf@turing.bio>
2025-10-31 14:58:55 +0000

    Docs (!fatal-exception)

--- lib/FatalException.h
+++ lib/FatalException.h
@@ -5,6 +5,8 @@
 /**
  * @brief Exception class to denote an internal logic error; a bad state from which
  * execution should not continue, but due to programmer error as opposed to user error.
+ * @detail May be used as a release-friendly `assert`; which can hault current execution
+ * path without causing a crash of the DB server
  */
 class FatalException : public TuringException {
 public:
