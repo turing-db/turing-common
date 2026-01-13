c02ccc9fd051e3bf6fd7ecf0d81bdb13de142975
Remy Boutonnet <remy.boutonnet@turing.bio>
2026-01-13 13:39:50 +0000

    Changes for macos build

--- lib/BasicResult.h
+++ lib/BasicResult.h
@@ -1,5 +1,9 @@
 #pragma once
 
+// Force expected-lite to use nonstd::expected instead of std::expected
+// to maintain compatibility with code using get_unexpected()
+#define nsel_CONFIG_SELECT_EXPECTED 1
+
 #include <expected.hpp>
 
 template <class TValue, class TError>
